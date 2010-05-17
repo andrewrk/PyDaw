165f5c53a4d07d1d271af4c6207f759be3ab04d8
Andrew Kelley <superjoe30@gmail.com>
2010-05-17 02:55:15 -0700

    python module now builds and is importable

--- FlpModule.cpp
+++ FlpModule.cpp
@@ -9,86 +9,14 @@ typedef struct {
     PyObject * x_attr; // attributes dictionary
 } FlpObject;
 
-staticforward PyTypeObject Flp_Type;
-
-#define FlpObject_Check(v) ((v)->ob_type == &Flp_Type)
-
-static FlpObject *
-newFlpObject(arg)
-    PyObject * arg;
-{
-    FlpObject * self;
-    self = PyObject_NEW(FlpObject, &Flp_Type);
-    if (self == NULL)
-        return NULL;
-    self->x_attr = NULL;
-    return self;
+extern "C" {
+    static void Flp_dealloc(FlpObject * self);
+    static PyObject * Flp_getattr(FlpObject * self, char * name);
+    static int Flp_setattr(FlpObject * self, char * name, PyObject * v);
+    DL_EXPORT(void) initflp();
 }
 
-// Flp methods
-
-static void
-Flp_dealloc(self)
-    FlpObject * self;
-{
-    Py_XDECREF(self->x_attr);
-    PyMem_DEL(self);
-}
-
-static PyObject *
-Flp_demo(self, args)
-    FlpObject * self;
-    PyObject * args;
-{
-    if (! PyArg_ParseTuple(args, ""))
-        return NULL;
-    Py_INCREF(Py_None);
-    return Py_None;
-}
-
-static PyMethodDef Flp_methods[] = {
-    {"demo",    (PyCFunction)Flp_demo,  1},
-    {NULL,      NULL} // sentinel
-};
-
-static PyObject *
-Flp_getattr(self, name)
-    FlpObject * self;
-    char * name;
-{
-    if (self->x_attr != NULL) {
-        PyObject * v = PyDict_GetItemString(self->x_attr, name);
-        if (v != NULL) {
-            Py_INCREF(v);
-            return v;
-        }
-    }
-    return Py_FindMethod(Flp_methods, (PyObject *)self, name);
-}
-
-static int
-Flp_setattr(self, name, v)
-    FlpObject *self;
-    char *name;
-    PyObject *v;
-{
-    if (self->x_attr == NULL) {
-        self->x_attr = PyDict_New();
-        if (self->x_attr == NULL)
-            return -1;
-    }
-    if (v == NULL) {
-        int rv = PyDict_DelItemString(self->x_attr, name);
-        if (rv < 0)
-            PyErr_SetString(PyExc_AttributeError,
-                    "delete non-existing Flp attribute");
-        return rv;
-    }
-    else
-        return PyDict_SetItemString(self->x_attr, name, v);
-}
-
-statichere PyTypeObject Flp_Type = {
+static PyTypeObject Flp_Type = {
     /* The ob_type field must be initialized in the module init function
      * to be portable to Windows without using C++. */
     PyObject_HEAD_INIT(NULL)
@@ -108,30 +36,74 @@ statichere PyTypeObject Flp_Type = {
     0,          /*tp_as_mapping*/
     0,          /*tp_hash*/
 };
-/* --------------------------------------------------------------------- */
 
-/* Function of two integers returning integer */
+#define FlpObject_Check(v) ((v)->ob_type == &Flp_Type)
 
-static PyObject *
-xx_foo(self, args)
-    PyObject *self; /* Not used */
-    PyObject *args;
+static FlpObject * newFlpObject(PyObject * arg)
 {
-    long i, j;
-    long res;
-    if (!PyArg_ParseTuple(args, "ll", &i, &j))
+    FlpObject * self;
+    self = PyObject_NEW(FlpObject, &Flp_Type);
+    if (self == NULL)
         return NULL;
-    res = i+j; /* XXX Do something here */
-    return PyInt_FromLong(res);
+    self->x_attr = NULL;
+    return self;
 }
 
+// Flp methods
+
+static void Flp_dealloc(FlpObject * self)
+{
+    Py_XDECREF(self->x_attr);
+    PyMem_DEL(self);
+}
+
+static PyObject * Flp_demo(FlpObject * self, PyObject * args)
+{
+    if (! PyArg_ParseTuple(args, ""))
+        return NULL;
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyMethodDef Flp_methods[] = {
+    {"demo",    (PyCFunction)Flp_demo,  1},
+    {NULL,      NULL} // sentinel
+};
+
+static PyObject * Flp_getattr(FlpObject * self, char * name)
+{
+    if (self->x_attr != NULL) {
+        PyObject * v = PyDict_GetItemString(self->x_attr, name);
+        if (v != NULL) {
+            Py_INCREF(v);
+            return v;
+        }
+    }
+    return Py_FindMethod(Flp_methods, (PyObject *)self, name);
+}
+
+static int Flp_setattr(FlpObject * self, char * name, PyObject * v)
+{
+    if (self->x_attr == NULL) {
+        self->x_attr = PyDict_New();
+        if (self->x_attr == NULL)
+            return -1;
+    }
+    if (v == NULL) {
+        int rv = PyDict_DelItemString(self->x_attr, name);
+        if (rv < 0)
+            PyErr_SetString(PyExc_AttributeError,
+                    "delete non-existing Flp attribute");
+        return rv;
+    }
+    else
+        return PyDict_SetItemString(self->x_attr, name, v);
+}
+/* --------------------------------------------------------------------- */
 
 /* Function of no arguments returning new Flp object */
 
-static PyObject *
-xx_new(self, args)
-    PyObject *self; /* Not used */
-    PyObject *args;
+static PyObject * flp_new(PyObject * self, PyObject * args)
 {
     FlpObject *rv;
     
@@ -143,60 +115,17 @@ xx_new(self, args)
     return (PyObject *)rv;
 }
 
-/* Example with subtle bug from extensions manual ("Thin Ice"). */
-
-static PyObject *
-xx_bug(self, args)
-    PyObject *self;
-    PyObject *args;
-{
-    PyObject *list, *item;
-    
-    if (!PyArg_ParseTuple(args, "O", &list))
-        return NULL;
-    
-    item = PyList_GetItem(list, 0);
-    /* Py_INCREF(item); */
-    PyList_SetItem(list, 1, PyInt_FromLong(0L));
-    PyObject_Print(item, stdout, 0);
-    printf("\n");
-    /* Py_DECREF(item); */
-    
-    Py_INCREF(Py_None);
-    return Py_None;
-}
-
-/* Test bad format character */
-
-static PyObject *
-xx_roj(self, args)
-    PyObject *self; /* Not used */
-    PyObject *args;
-{
-    PyObject *a;
-    long b;
-    if (!PyArg_ParseTuple(args, "O#", &a, &b))
-        return NULL;
-    Py_INCREF(Py_None);
-    return Py_None;
-}
-
-
 /* List of functions defined in the module */
 
-static PyMethodDef xx_methods[] = {
-    {"roj",     xx_roj,     1},
-    {"foo",     xx_foo,     1},
-    {"new",     xx_new,     1},
-    {"bug",     xx_bug,     1},
+static PyMethodDef flp_methods[] = {
+    {"new",     flp_new,     1},
     {NULL,      NULL}       /* sentinel */
 };
 
 
-/* Initialization function for the module (*must* be called initxx) */
+/* Initialization function for the module (*must* be called initflp) */
 
-DL_EXPORT(void)
-initxx()
+DL_EXPORT(void) initflp()
 {
     PyObject *m, *d;
 
@@ -205,11 +134,10 @@ initxx()
     Flp_Type.ob_type = &PyType_Type;
 
     /* Create the module and add the functions */
-    m = Py_InitModule("xx", xx_methods);
+    m = Py_InitModule("flp", flp_methods);
 
     /* Add some symbolic constants to the module */
     d = PyModule_GetDict(m);
-    ErrorObject = PyErr_NewException("xx.error", NULL, NULL);
+    ErrorObject = PyErr_NewException("flp.error", NULL, NULL);
     PyDict_SetItemString(d, "error", ErrorObject);
 }
-
