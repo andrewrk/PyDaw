4048a32f3565eee8f176f2e59ab927c3e4b9ab07
Andrew Kelley <superjoe30@gmail.com>
2010-06-04 09:01:43 -0700

    add a method for getting a list of daw ids

--- daw/__init__.py
+++ daw/__init__.py
@@ -1,27 +1,32 @@
+from .flstudio import FlStudio
+from .lmms import Lmms
+from .dummy import Dummy
+
 __version__ = '0.2'
+
+_class_tuple = (
+    FlStudio,
+    Lmms,
+)
 
 def dawFor(projectFile):
     """
     Picks the daw class that is best suited for opening projectFile.
     """
-    from .flstudio import FlStudio
-    from .lmms import Lmms
-    from .dummy import Dummy
 
-    class_tuple = (
-        FlStudio,
-        Lmms,
-    )
-
-    for item in class_tuple:
+    for item in _class_tuple:
         if item.isValid(projectFile):
             return item
 
     from exceptions import LoadError
     raise LoadError("File not recognized with any studio.")
 
+def dawIds():
+    return [X.identifier for X in _class_tuple]
+
 def load(projectFile):
     DawClass = dawFor(projectFile)
     f = DawClass()
     f.load(projectFile)
     return f
+
